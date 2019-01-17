a78f47e38a8df94d04afaeed7a88cf3c0fdd0012
Brandon Lent <blent1050@yahoo.com>
2019-01-17 13:29:19 -0500

    styled components added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostComponents/PostsPage';
 import {Container} from 'reactstrap';
-import authenticate from './components/authentication/authenticate';
+
+import styled from 'styled-components';
+
 
 class App extends Component {
   constructor(props){
