48b9c18871a460bc4f11f196c354168d497c0f11
Brandon Lent <blent1050@yahoo.com>
2019-01-17 16:53:14 -0500

    All styling transitioned to stled-components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 import PostsPage from './components/PostComponents/PostsPage';
 import {Container} from 'reactstrap';
 
 import styled from 'styled-components';
+
 
 
 class App extends Component {
@@ -13,13 +13,87 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <AppStyle>
         <Container>
           <PostsPage />
         </Container>
-      </div>
+      </AppStyle>
     );
   }
 }
 
 export default App;
+//Styling
+const AppStyle = styled.div`
+  text-align: center;
+  width: 75%;
+  max-width: 1024px;
+  margin: 0 auto;
+  min-width: 676px;
+  i{
+    font-size: 28px;
+    margin: 0 5px;
+  }
+  .post{
+    border: 1px solid rgba(128, 128, 128, 0.363);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 50px;
+  }
+  
+  .post-header{
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+  
+  .thumbnail{
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    margin-right: 15px;
+  }
+  
+  .post-image{
+    width: 100%;
+    height: auto;
+  }
+  
+  .post-icons{
+    text-align: left;
+    margin-top: 20px;
+  }
+  .likes{
+    text-align: left;
+  }
+  .comment{
+    text-align: left;
+  }
+  
+  .line{
+    width: 90%;
+    color: rgba(208, 205, 205, 0.16);
+    margin: 30px auto;
+  }
+  
+  .add-comment{
+    border: none;
+    display: flex;
+    width: 95%;
+    height: 43px;
+    font-size: 20px;
+  }
+  .add-comment:focus{
+    outline-width: 0;
+  }
+  .timestamp{
+    text-align: left;
+    font-size: 12px;
+    color: gray;
+  }
+  
+  .icon-interactable{
+    cursor: pointer;
+  }
+`
--- instagram/src/components/SearchBarComponent/SearchBar.js
+++ instagram/src/components/SearchBarComponent/SearchBar.js
@@ -1,8 +1,55 @@
 import React from 'react'
-import './SearchBarStyling.css'
+import styled from 'styled-components';
 
+const SearchBarStyling = styled.div`
+.search {
+  position: relative;
+  color: #aaa;
+  font-size: 16px;
+}
+
+.search input {
+  width: 200px;
+  height: 32px;
+  background: #fcfcfc;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.search input { text-indent: 70px;
+}
+.search .fa-search { 
+  display: inline;
+  position: relative;
+  left:65px;
+}
+
+.searchBar{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+    margin: 25px 0;
+    justify-content: space-between;
+}
+.searchBar div{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.searchBar img{
+  width: 120px;
+  
+}
+.logo img{
+  margin-left: 10px;
+  padding-left: 10px;
+  border-left: 1px solid rgba(128, 128, 128, 0.482);
+}
+`
 export default function SearchBar(props) {
   return (
+    <SearchBarStyling>
     <div className='searchBar'>
       <div className='logo'>
         <i className="fab fa-instagram"/>
@@ -25,6 +72,7 @@ export default function SearchBar(props) {
         <i className="far fa-user"></i>
       </div>
     </div>
+    </SearchBarStyling>
   )
 }
 
